2f0033604b8c7289947b82fa4c5a972540f7ac82
ZhaoJiaXuan-Hub <2687409344@qq.com>
2023-11-07 21:29:25 +0800

    增加swoole-http

---
+++ src/swoole/Context.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ethereal\swoole;
+
+use Swoole\Coroutine;
+
+class Context
+{
+    protected static $pool = [];
+
+    static function get($key)
+    {
+        $cid = Coroutine::getuid();
+        // 获取当前协程id
+        if ($cid < 0) {
+            return null;
+        }
+        if (isset(self::$pool[$cid][$key])) {
+            return self::$pool[$cid][$key];
+        }
+        return null;
+    }
+
+    static function put($key, $item)
+    {
+        $cid = Coroutine::getuid();
+        if ($cid > 0) {
+            self::$pool[$cid][$key] = $item;
+        }
+    }
+
+    // 删除当前协程的变量
+    static function delete($key = null)
+    {
+        $cid = Coroutine::getuid();
+        if ($cid > 0) {
+            if ($key) {
+                unset(self::$pool[$cid][$key]);
+            } else {
+                unset(self::$pool[$cid]);
+            }
+        }
+    }
+}
